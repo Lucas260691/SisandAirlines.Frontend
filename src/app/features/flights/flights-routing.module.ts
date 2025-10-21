f7be3d903cf46b58aea334bf189ed90b7187939c
Lucas <lucasmeinen@yahoo.com.br>
2025-10-21 11:03:29 -0300

    feat:Integração de busca e listagem de voos (Front-end)

--- src/app/features/flights/flights-routing.module.ts
+++ src/app/features/flights/flights-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlightsListComponent } from './flights-list/flights-list.component';
+import { FlightsSearchComponent } from './flights-search/flights-search.component';
 
 const routes: Routes = [
-  { path: '', component: FlightsListComponent }
+  { path: '', component: FlightsSearchComponent },
+  { path: 'results', component: FlightsListComponent }
 ];
 
 @NgModule({
--- src/app/features/flights/flights.module.ts
+++ src/app/features/flights/flights.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlightsRoutingModule } from './flights-routing.module';
 import { FlightsListComponent } from './flights-list/flights-list.component';
+import { FlightsSearchComponent } from './flights-search/flights-search.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FlightsRoutingModule,
-    FlightsListComponent
+    FlightsListComponent,
+    FlightsSearchComponent
   ]
 })
 export class FlightsModule {}
